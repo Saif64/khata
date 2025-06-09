1f43501fc96c233917338fe2b8552d38fb643cad
saif64s <my@wo.rk>
2025-06-09 15:08:49 +0600

    Move the main.dart logic into separate file

--- presentation/lib/main.dart
+++ presentation/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:presentation/core/theme/app_theme.dart';
+import 'package:presentation/src/features/auth/provider/auth.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/di/injection.dart';
 import 'core/routes.dart';
+import 'src/auth_wrapper.dart'; // Import the AuthWrapper
+// Auth BLoC and Screens
+import 'src/features/auth/presentation/screens/sign_in_screen.dart';
+import 'src/features/auth/presentation/screens/sign_up_screen.dart';
+import 'src/features/home/presentation/screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,13 +36,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Khata',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      routes: Routes.getRoutes(),
-      initialRoute: '/',
+    return BlocProvider(
+      create: (context) => di<AuthBloc>()..add(const AuthCheckRequested()),
+      child: MaterialApp(
+        title: 'Khata',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        routes: {
+          '/': (context) => const AuthWrapper(), // Initial route
+          Routes.SIGN_IN: (context) => const SignInScreen(),
+          Routes.SIGN_UP: (context) => const SignUpScreen(),
+          Routes.HOME: (context) => const HomeScreen(),
+        },
+        initialRoute: '/',
+      ),
     );
   }
 }
