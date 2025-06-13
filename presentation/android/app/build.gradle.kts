2dc6531e121186fce4a4973ce04f3ee047780ea4
saif64s <my@wo.rk>
2025-06-13 20:40:54 +0600

    Add App icon, splash and config android

--- presentation/android/app/build.gradle.kts
+++ presentation/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.presentation"
+    namespace = "com.pixel.khata"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.presentation"
+        applicationId = "com.pixel.khata"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- presentation/android/app/src/main/kotlin/com/pixel/khata/MainActivity.kt
+++ presentation/android/app/src/main/kotlin/com/pixel/khata/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.presentation
+package com.pixel.khata
 
 import io.flutter.embedding.android.FlutterActivity
 
